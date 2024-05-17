7dc17c5ee21ea285fa4cfbf9f837ff84f15b008f
jfrisina <jaki.frisina@gmail.com>
2024-05-17 15:40:48 -0700

    finish assignment

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -38,7 +38,79 @@ menuLinks.forEach(function(link) {
   topMenuEl.appendChild(a);
 });
 
-// Part 4 -------------------------------------------
-console.log("Part 4 -----------------------------------")
+// Creating the Submenu -------------------------------------------
+console.log("Creating the Submenu -----------------------------------")
 
-console.log("It looks like this doesn't actually have a fourth part? Please let me know if I misunderstood this!")
+let subMenuEl = document.getElementById("sub-menu");
+
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+// Adding Menu + Submenu Interaction -------------------------------------------
+console.log("Adding Menu + Submenu Interaction -----------------------------------")
+
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+
+function buildSubmenu(subLinks) {
+  subMenuEl.innerHTML = "";
+  for (let link of subLinks) {
+    let a = document.createElement("a");
+    a.setAttribute("href", link.href);
+    a.textContent = link.text;
+    subMenuEl.appendChild(a);
+  }
+};
+
+// event delegation!!
+topMenuEl.addEventListener("mouseover", function(event) {
+  if (event.target.tagName === "A" || event.target.textContent === "about") {
+    event.preventDefault();
+    mainEl[0].innerHTML = `<h1>${event.target.textContent}</h1>`
+    topMenuEl.querySelectorAll("a").forEach(function(link) {
+      link.classList.remove("active");
+    });
+
+    if (!event.target.classList.contains("active")) {
+      let linkObject = menuLinks.find(link =>link.text === event.target.textContent);
+      if (linkObject && linkObject.subLinks) {
+        subMenuEl.style.top = "100%";
+        event.target.classList.toggle("active");
+        buildSubmenu(linkObject.subLinks);
+      } else
+      subMenuEl.style.top = "0";
+    }
+  }
+});
+
+subMenuEl.addEventListener("mouseover", function(event) {
+  event.preventDefault();
+  if (event.target.tagName !== "A") {
+    return;
+  } else {
+      subMenuEl.style.top = "0";
+      console.log(event.target.textContent);
+      document.querySelectorAll("a").forEach(function(link) {
+    topMenuEl.classList.remove("active");
+  });
+    mainEl[0].innerHTML = `<h1>${event.target.textContent}</h1>`;
+  }
+});
